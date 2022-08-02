bf978926ca192ed6f65d804433563788ea8920c2
Asher <jasherleslie@gmail.com>
2022-08-03 09:45:57 +1000

    Add base working config.

---
+++ after/plugin/sourcery.lua
@@ -0,0 +1,34 @@
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+local server_name = 'sourcery'
+
+configs[server_name] = {
+    default_config = {
+        cmd = {server_name, 'lsp'},
+        filetypes = {'python'},
+        init_options = {
+            editor_version = 'vim',
+            extension_version = 'vim.lsp',
+            token = 'user_rZKS__MPhBGgrfMahsWEhAiYkbdM54N67aZPrgbSYV89NrKq5wyAIEMjwbw'
+        },
+        root_dir = function(fname)
+            local root_files = {
+                'pyproject.toml',
+                'setup.py',
+                'setup.cfg',
+                'requirements.txt',
+                'Pipfile'
+            }
+            return util.root_pattern(unpack(root_files))(fname) or
+                       util.find_git_ancestor(fname) or util.path.dirname(fname)
+        end
+    },
+    docs = {
+        description = [[
+https://github.com/sourcery-ai/sourcery
+
+Refactor Python instantly using the power of AI.
+]]
+    }
+}
